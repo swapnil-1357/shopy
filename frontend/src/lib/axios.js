8f4d11b5779a5c196b8d207bd5b62d6229509570
asur1357 <sukanil1357@gmail.com>
2025-07-14 14:07:03 +0530

    🚀ready to deploy

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000/api', // Use your deployed backend URL here 'https://shopy-1-jf5l.onrender.com/api' ||
+    baseURL: 'https://shopy-1-jf5l.onrender.com/api', //'http://localhost:5000/api', // Use your deployed backend URL here ||
     withCredentials: false,
     headers: {
         'Content-Type': 'application/json',
