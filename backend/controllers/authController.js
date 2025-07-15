8a3b626f134035855e99a5533918794e25b21743
asur1357 <sukanil1357@gmail.com>
2025-07-15 18:07:53 +0530

    🚀ready to deploy

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -146,7 +146,7 @@ export const logout = (req, res) => {
   res.clearCookie('token', {
     httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-    sameSite: 'lax',
+      sameSite: 'None',
   });
   res.status(200).json({ message: 'Logged out' });
 };
